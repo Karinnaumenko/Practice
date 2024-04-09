59154e0e80203f54fe77bb9b0bc344dd79b5b2b0
Katrin <naumenko.katerina2003@yandex.ru>
2024-04-09 13:25:10 +0700

    программа новая самост работа

---
+++ cmdargs.c
@@ -0,0 +1,21 @@
+//новая программа
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	int i;
+	printf("Количество аргументов: %d\n\n", argc);
+	for(i=0; i<argc; i++) {
+		printf("Аргумент %d: %s\n", i, argv[i]);
+	}
+	printf("\n");
+	i=0;
+	while (envp[i]!=NULL) {
+		printf("%s\n", envp[i]);
+		i++;
+	}
+	return 0;
+}
