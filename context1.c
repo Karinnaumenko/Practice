8055fcd4b873f000a50898fc238e52ee46fb1d54
Katrin <naumenko.katerina2003@yandex.ru>
2024-04-16 12:56:54 +0700

    Добавили коментарии в  исходный код

--- context1.c
+++ context1.c
@@ -1,12 +1,15 @@
+//Подключение библиотек
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+//Главная функция программы
 int main(int argc, char *argv[], char *envp[]){
 	int result;
+	//Запуск программы cat с аргументом otch.txt
 	result = execle("/bin/cat", "/bin/cat", "otch.txt", 0, envp);
+	//printf() выполняется только в случае ошибки вызова execle()
 	printf("Ошибка при выполнении системного вызова execle()\n");
 	exit(-1);
-	return 0;
+	return 0; //никогда не выполняется
 }
