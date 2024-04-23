13c3a572a0cbea93a134c046acd2c65fa6de2849
Katrin <naumenko.katerina2003@yandex.ru>
2024-04-23 15:31:33 +0700

    Добавлен комемнатрий в программу, читающую file1.txt

--- connect2.c
+++ connect2.c
@@ -2,21 +2,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+//Библиотека управления файлами 
 #include <fcntl.h>
 int main() {
 	int fd;
 	size_t size;
+//Инициализируем  массив символов, чтобы туда записать информацию из файла
 	char string[17];
 	if((fd = open("file1.txt", O_RDONLY))<0) {
 		printf("Ошибка при открытии файла на чтение\n");
 		exit(-1);
 	}
+//Чтение информации из файла
 	size =read(fd, string, 17);
+//Проверка количества прочитанных байт
 	if (size !=17) {
 		printf("Не удалось записать всю строку из файла\n");
 		exit(-1);
 	}
-	printf("%s", string);
+//Печать строки
+	printf("%s\n", string);
+//Закрытие файла
 	if(close(fd) < 0) {
 		printf("Не удалось закрыть файл\n");
 		exit(-1);
