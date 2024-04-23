08d85e7f451f2a629900bc023ec2ef7d353db7e4
Katrin <naumenko.katerina2003@yandex.ru>
2024-04-23 15:34:55 +0700

    Добавлены комментарии в программы работы с файлами

--- connect1.c
+++ connect1.c
@@ -3,19 +3,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+//Подключение библиотеки для управления файлами
 int main() {
-	int fd;
-	size_t size;
+	int fd;//Файловый дескриптор
+	size_t size;//Размер строки в байтах
 	char string[]="Group 3.120.2.21";
+//Открытие файла на запись, если  файла не существует; то он будет создан с правами доступа 0666
 	if((fd = open("file1.txt", O_WRONLY|O_CREAT, 0666))<0) {
 		printf("Ошибка при открытии файла на запись\n");
 		exit(-1);
 	}
-	size = (fd, string, 17);
+//Чтение из файла в строку string
+	size =write(fd, string, 17);
 	if (size !=17) {
 		printf("Не удалось записать всю строку в файл\n");
 		exit(-1);
 	}
+//Закрытие файла
 	if(close(fd) < 0) {
 		printf("Не удалось закрыть файл\n");
 		exit(-1);
